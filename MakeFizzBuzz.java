f4ff89349f0d82b9094678a7df7c3b766e47bae2
Sparky <59151395+Sparks29032@users.noreply.github.com>
2022-03-06 11:27:07 -0500

    Update MakeFizzBuzz.java

--- MakeFizzBuzz.java
+++ MakeFizzBuzz.java
@@ -7,11 +7,13 @@ public class MakeFizzBuzz {
 		generateFizzBuzz("FizzBuzz");
 	}
 	
+	//makes a .java file that will have the one-line FizzBuzz solution
 	public static void generateFizzBuzz(String className) throws IOException {
 		PrintWriter pw = new PrintWriter(new File(className + ".java"));
 		pw.println("public class " + className + "{");
 		pw.println("\tpublic static void main(String[] args) {");
 		pw.print("\t\tSystem.out.println(\"");
+		//simple O(n) solution to FizzBuzz
 		for (int i = 1; i <= 100; i++) {
 			if (i % 15 == 0) {
 				pw.print("FizzBuzz\\n");
